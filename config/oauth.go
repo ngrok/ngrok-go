f979fc3da019a8d1fa1594c0dd723a3468e4fb96
Josh Robson Chase <joshrc@ngrok.com>
2023-03-03 11:53:52 -0500

    oauth: expose client id and secret configuration

--- config/oauth.go
+++ config/oauth.go
@@ -1,6 +1,8 @@
 package config
 
-import "golang.ngrok.com/ngrok/internal/pb"
+import (
+	"golang.ngrok.com/ngrok/internal/pb"
+)
 
 type OAuthOption func(cfg *oauthOptions)
 
@@ -14,12 +16,30 @@ type oauthOptions struct {
 	AllowDomains []string
 	// OAuth scopes to request from the provider.
 	Scopes []string
+	// OAuth custom app ID
+	ClientID string
+	// OAuth custom app secret
+	ClientSecret pb.ObfuscatedString
 }
 
 // Construct a new OAuth provider with the given name.
 func oauthProvider(name string) *oauthOptions {
 	return &oauthOptions{
 		Provider: name,
+	}
+}
+
+// WithOAuthClientID provides a client ID for custom OAuth apps.
+func WithOAuthClientID(id string) OAuthOption {
+	return func(cfg *oauthOptions) {
+		cfg.ClientID = id
+	}
+}
+
+// WithOAuthClientSecret provides a client secret for custom OAuth apps.
+func WithOAuthClientSecret(secret string) OAuthOption {
+	return func(cfg *oauthOptions) {
+		cfg.ClientSecret = pb.ObfuscatedString(secret)
 	}
 }
 
@@ -51,6 +71,8 @@ func (oauth *oauthOptions) toProtoConfig() *pb.MiddlewareConfiguration_OAuth {
 
 	return &pb.MiddlewareConfiguration_OAuth{
 		Provider:     string(oauth.Provider),
+		ClientId:     oauth.ClientID,
+		ClientSecret: pb.ObfuscatedString(oauth.ClientSecret),
 		AllowEmails:  oauth.AllowEmails,
 		AllowDomains: oauth.AllowDomains,
 		Scopes:       oauth.Scopes,
