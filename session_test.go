b2cbb7f9bb4e0d474f2f67fef9be6ec54ed3d7c8
Natasha Jarus <natasha@ngrok.com>
2023-04-20 11:53:03 -0700

    session: add optional comments to user agent string

--- session_test.go
+++ session_test.go
@@ -7,9 +7,16 @@ import (
 )
 
 func TestUserAgent(t *testing.T) {
-	s := (&clientInfo{"library/official/go", "1.2.3"}).ToUserAgent()
+	s := (&clientInfo{Type: "library/official/go", Version: "1.2.3"}).ToUserAgent()
 	require.Equal(t, s, "library-official-go/1.2.3")
 
-	s = (&clientInfo{"some@funky☺user agent", "№1.2.3"}).ToUserAgent()
+	s = (&clientInfo{Type: "some@funky☺user agent", Version: "№1.2.3"}).ToUserAgent()
 	require.Equal(t, s, "some#funky#user#agent/#1.2.3")
+
+	s = (&clientInfo{
+		Type:     "agent/official/go",
+		Version:  "3.2.1",
+		Comments: []string{"{\"ProxyType\": \"socks5\", \"ConfigVersion\": \"2\"}"},
+	}).ToUserAgent()
+	require.Equal(t, s, "agent-official-go/3.2.1 ({\"ProxyType\": \"socks5\", \"ConfigVersion\": \"2\"})")
 }
