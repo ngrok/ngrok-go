98c62b9c0ed5e141cd8685f5c60562d1d2e1e2b9
bobzilladev <bob@ngrok.com>
2023-10-16 11:01:13 -0400

    Docs feedback

--- config/tls.go
+++ config/tls.go
@@ -18,7 +18,7 @@ func (of tlsOptionFunc) ApplyTLS(cfg *tlsOptions) {
 	of(cfg)
 }
 
-// Construct a new set of TLS tunnel options.
+// TLSEndpoint constructs a new set options for a TLS endpoint.
 func TLSEndpoint(opts ...TLSEndpointOption) Tunnel {
 	cfg := tlsOptions{}
 	for _, opt := range opts {
