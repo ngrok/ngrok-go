44f2da94707b575dc8fd8fc6503b95c10c487531
kc <kc@ngrok.com>
2024-07-17 14:54:31 +0000

    Keep incorrect println and fix elsewhere

--- config/policy.go
+++ config/policy.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/inconshreveable/log15"
 	"gopkg.in/yaml.v3"
 
 	po "golang.ngrok.com/ngrok/policy"
@@ -78,8 +77,7 @@ func (p *policy) ApplyTCP(opts *tcpOptions) {
 
 // policyToString converts the policy into a JSON string representation. This is to help remap Policy to TrafficPolicy.
 func policyToString(p *policy) string {
-	logger := log15.New()
-	logger.Warn("WithPolicy has been deprecated. Please use WithPolicyString instead, as WithPolicy will stop working soon.")
+	fmt.Println("WithPolicy has been deprecated. Please use WithPolicyString instead, as WithPolicy will stop working soon.")
 
 	val, err := json.Marshal(p)
 	if err != nil {
